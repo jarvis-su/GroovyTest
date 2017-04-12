e80634e866ebcb13fe07c49bee0c3c6de1f17913
JarvisSu <jarvis.su@189.cn>
2017-04-12 23:05:17 +0800

    add hello class

---
+++ src/T.groovy
@@ -0,0 +1,8 @@
+/**
+ * Created by Jarvis on 12/04/2017.
+ */
+class T {
+    void hello() {
+        println("hello ")
+    }
+}
---
+++ src/main.groovy
@@ -0,0 +1,6 @@
+/**
+ * Created by Jarvis on 12/04/2017.
+ */
+
+app= new T()
+app.hello()
